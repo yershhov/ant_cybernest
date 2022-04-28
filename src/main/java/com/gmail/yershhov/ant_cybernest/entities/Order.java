594a7d4846b7ff79de9c1f65d530319c03bbcc89
yershhov <yershhov@gmail.com>
2022-04-28 13:35:32 +0200

    just combined name and surname fields into a single full name field

--- src/main/java/com/gmail/yershhov/ant_cybernest/entities/Order.java
+++ src/main/java/com/gmail/yershhov/ant_cybernest/entities/Order.java
@@ -2,8 +2,7 @@ package com.gmail.yershhov.ant_cybernest.entities;
 
 public class Order {
     private String game;
-    private String name;
-    private String surname;
+    private String fullName;
     private String email;
     private String phone;
     private String experience;
@@ -30,13 +29,6 @@ public class Order {
         this.game = game;
     }
 
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
 
     public String getPhone() {
         return phone;
@@ -46,12 +38,12 @@ public class Order {
         this.phone = phone;
     }
 
-    public String getName() {
-        return name;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getEmail() {
