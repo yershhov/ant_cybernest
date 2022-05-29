24b08ef4eca5c11d3672c5707e7e4eb8a09c97ba
yershhov <yershhov@gmail.com>
2022-05-30 00:46:41 +0200

    minor

--- src/main/java/com/gmail/yershhov/ant_cybernest/entities/Order.java
+++ src/main/java/com/gmail/yershhov/ant_cybernest/entities/Order.java
@@ -4,13 +4,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Order {
+    @Id
+    @SequenceGenerator(
+            name = "order_sequence",
+            sequenceName = "order_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "order_sequence"
+    )
+    private Integer orderId;
+
     private Integer userId;
     private String game;
-    private Integer orderId;
     private String userExperience;
 }
 
